524724d1e5420c54b8689978742ccd5c9f114a9f
Livne Lang <livnelang@gmail.com>
2015-05-04 15:56:17 +0300

    First Commit - JSON File - WS File

---
+++ AuthorsApp.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var url = require('url');
+var app = express();
+var fs = require('fs');
+
+app.get('/', function(req,res) {
+	//res.send('HelloWorld');
+	// Gets Json FileS
+
+	var objectData;
+	fs.readFile('./Books.js', 'utf8', function (err, data) {
+	  //if (err) throw err;
+	  console.log(data);
+	  objectData = JSON.parse(data.toString());
+	  	res.send(objectData);
+});
+
+	console.log(objectData);
+});
+
+// Get Functions Routes
+
+app.listen(8080);
+console.log('Listening on port 8080 ..');
---
+++ Books.js
@@ -0,0 +1,23 @@
+{
+	"authors":[
+
+	{"name": "Meir", 
+	"lastname": "Shalev",
+	 "id": 5341279,
+	"books": [ {"name": "A Boy & A Pigeon","pages":224, "year":1987},
+				{"name": "Russian Roman","pages":300, "year":1956} ]    
+
+	},
+
+	{"name": "Amos", 
+	"lastname": "Shalev",
+	 "id": 5341279,
+	"books": [ {"name": "A Boy & A Pigeon","pages":224, "year":1987},
+				{"name": "Russian Roman","pages":300, "year":1956} ]    
+	}
+
+
+]
+
+
+}
